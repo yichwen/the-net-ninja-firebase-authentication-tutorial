5249629a5d9ec83bd9ca663c199ad0c18cd7e86a
LIM YI CHWEN <ben.lim@solderfieldgroup.com>
2020-08-04 11:23:12 +0800

    course complete

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+
+
+  // check request is made by admin
+  if (context.auth.token.admin !== true) {
+    return {
+      error: 'only admins can add other admins, sucker'
+    }
+  }
+
+  // get user and add custom claim (admin)
+  return admin.auth().getUserByEmail(data.email).then((user) => {
+    return admin.auth().setCustomUserClaims(user.uid, {
+      admin: true
+    });
+  }).then(() => {
+    return {
+      message: `Success! ${data.email} has been made an admin`
+    }
+  }).catch((err) => {
+    return err;
+  });
+});
